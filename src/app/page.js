1e4aed4c85053b1ad3f0da0c533b6494b2830ed5
Leonardo <leocolgucci@gmail.com>
2025-09-19 14:17:31 +0200

    resettati input della form all'inserimento, aggiunto anche required giorno settimana

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,19 @@ export default function Home() {
   })
 
 
-  const handleCloseModal = () => setIsModalOpen(false);
+  const handleCloseModal = () => {
+    setIsModalOpen(false)
+    setTask({
+      dayWeek: '',
+      lection: '',
+      startTime: '',
+      endTime: '',
+      description: '',
+      class: '',
+      professor: ''
+    });
+  };
+
   const handleOpenModal = () => setIsModalOpen(true)
   const handleChangeForm = (e) => {
     const { name, value } = e.target;
@@ -31,7 +43,6 @@ export default function Home() {
 
   const handleRemove = (id) => {
     const newLezioni = lezioni.filter((lezione) => lezione.id !== id);
-
     setLezioni(newLezioni)
     localStorage.setItem('lezioni', JSON.stringify(newLezioni));
   }
@@ -64,7 +75,6 @@ export default function Home() {
       setGiornoSelezionato(null);
     }
 
-
     if (savedLezioni) {
       setLezioni(JSON.parse(savedLezioni));
     }
@@ -83,7 +93,7 @@ export default function Home() {
       <Modal isOpen={isModalOpen} close={handleCloseModal}>
         <form className="flex flex-col gap-5" method="post" onSubmit={handleForm}>
           <h2 className="text-2xl font-bold">Lezione</h2>
-          <select value={task.dayWeek} name="dayWeek" onChange={handleChangeForm} className="border-1 rounded border-gray-300 p-2 px-4">
+          <select value={task.dayWeek} name="dayWeek" onChange={handleChangeForm} className="border-1 rounded border-gray-300 p-2 px-4" required>
             <option value='' disabled>Giorno della settimana</option>
             <option value={1}>Lunedi</option>
             <option value={2}>Martedi</option>
@@ -92,19 +102,19 @@ export default function Home() {
             <option value={5}>Venerdi</option>
           </select>
 
-          <input type="text" name="lection" onChange={handleChangeForm} placeholder="Nome lezione" className="border-1 rounded border-gray-300 p-2 px-4" required />
+          <input type="text" value={task.lection} name="lection" onChange={handleChangeForm} placeholder="Nome lezione" className="border-1 rounded border-gray-300 p-2 px-4" required />
 
-          <input type="time" name="startTime" onChange={handleChangeForm} className="border-1 rounded border-gray-300 p-2 px-4 w-full" required />
-          <input type="time" name="endTime" onChange={handleChangeForm} className="border-1 rounded border-gray-300 p-2 px-4 w-full" required />
+          <input type="time" value={task.startTime || ''} name="startTime" onChange={handleChangeForm} className="border-1 rounded border-gray-300 p-2 px-4 w-full" required />
+          <input type="time" value={task.endTime || ''} name="endTime" onChange={handleChangeForm} className="border-1 rounded border-gray-300 p-2 px-4 w-full" required />
 
-          <textarea name="description" onChange={handleChangeForm} className="w-full border-1 border-gray-300 h-30 resize-none rounded p-2 px-4" placeholder="descrizione"></textarea>
-          <input type="text" name="class" onChange={handleChangeForm} placeholder="Aula" className="border-1 border-gray-300 rounded p-2 px-4" required />
-          <input type="text" name="professor" onChange={handleChangeForm} placeholder="Nome Professore" className="border-1 border-gray-300 rounded p-2 px-4" required />
+          <textarea value={task.description} name="description" onChange={handleChangeForm} className="w-full border-1 border-gray-300 h-30 resize-none rounded p-2 px-4" placeholder="descrizione"></textarea>
+          <input value={task.class} type="text" name="class" onChange={handleChangeForm} placeholder="Aula" className="border-1 border-gray-300 rounded p-2 px-4" required />
+          <input value={task.professor} type="text" name="professor" onChange={handleChangeForm} placeholder="Nome Professore" className="border-1 border-gray-300 rounded p-2 px-4" required />
 
           <button type="submit" className=" bg-(--bg-glossy) p-3 rounded-2xl font-bold">Aggiungi</button>
         </form>
       </Modal>
     </>
 
-  );
-}
+  )
+};
